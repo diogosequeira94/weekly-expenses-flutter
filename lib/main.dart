d8a3ef821ddace43118ad4130a183234c898b0c7
Diogo Sequeira <diogosequeira@hotmail.fr>
2020-08-25 18:08:45 +0100

    forced portrait mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:math';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_weekly_expenses/components/chart.dart';
 import 'package:flutter_weekly_expenses/components/transaction_form.dart';
 import 'components/transaction_list.dart';
@@ -10,6 +11,8 @@ main() => runApp(ExpensesApp());
 class ExpensesApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations(
+        [DeviceOrientation.portraitUp]);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: MyHomePage(),
